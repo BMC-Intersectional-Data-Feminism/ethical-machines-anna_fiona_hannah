dbdcad07d1e23882abc6f37a72af9c071012d16c
Fiona <fionashen729@outlook.com>
2025-03-19 18:47:06 +0000

    edited function

--- student_function.py
+++ student_function.py
@@ -27,15 +27,7 @@ def ped_and_animal(option1, option2):
             return selected, option2
         else:
             return selected, option1
-"""
-if option1[0] in urban_planning.all_non_vehicles: ## Check if option 1 is a non-vehicle, if so, pick that. 
-    return option1, option2
-elif option2[0] in urban_planning.all_non_vehicles:  ## If option 1 is not a non-vehicle, check if option 2 is. If so, pick that. 
-    return option2, option1
-else:
-    return option1, option2  # Default to first option if both are vehicles
-"""
-
+        
 # Run the activity using the example algorithm
 print("\n🔹 Running Example Algorithm: Always Pick Non-Vehicle 🔹")
 urban_planning.run_activity(num_scenarios=25, decision_function = ped_and_animal)
